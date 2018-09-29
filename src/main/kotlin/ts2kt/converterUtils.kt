8eec835de43856859089f8151d75a782c20a01e7
Eric Pabst <epabst@gmail.com>
2018-09-29 16:17:35 -0600

    Omit numeric literal properties since they cause Kotlin compile errors
    Fixes https://github.com/Kotlin/ts2kt/issues/108

--- src/main/kotlin/ts2kt/converterUtils.kt
+++ src/main/kotlin/ts2kt/converterUtils.kt
@@ -37,7 +37,8 @@ fun PropertyName.asString() = when (kind) {
         this.cast<Identifier>().unescapedText
     }
     SyntaxKind.NumericLiteral -> {
-        (this.cast<LiteralExpression>()).text
+        console.warn("ts2kt: numeric literals are not supported by KotlinJS so they will be omitted")
+        null
     }
     else -> {
         reportUnsupportedNode(this)
--- testData/class/variables/withNumberAsName.d.kt
+++ testData/class/variables/withNumberAsName.d.kt
@@ -1,8 +1,4 @@
 package withNumberAsName
 
 external interface IBar
-external open class Foo {
-    open var `200`: String = definedExternally
-    open var `300`: Number = definedExternally
-    open var `400`: IBar = definedExternally
-}
+external open class Foo
